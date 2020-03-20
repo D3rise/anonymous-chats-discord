5b090340dc9634f95fef96b3830cc2bea7b735b3
D3rise <derise2000@gmail.com>
2020-03-20 20:02:49 +0300

    Фикс ошибки, при которой отправленные файлы не сохранялись в бд #2

--- src/entity/Message.entity.ts
+++ src/entity/Message.entity.ts
@@ -31,7 +31,7 @@ export class Message {
   @Column({ nullable: true })
   content: string;
 
-  @Column("simple-array", { default: [] })
+  @Column("array", { default: [], nullable: true })
   attachmentUris: string[];
 
   @Column({ default: new Date() })
