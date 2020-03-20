6a4665d613af860d9651d78e3454c8c2c56f64f8
D3rise <derise2000@gmail.com>
2020-03-20 18:50:06 +0300

    Исправление бага с неработающими командами #2

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -2,9 +2,11 @@ import Listener from "../struct/Listener";
 import { Guild } from "discord.js";
 import { Repository, getRepository } from "typeorm";
 import { Guild as GuildEntity } from "../entity/Guild.entity";
+import { User } from "../entity/User.entity";
 
 class GuildCreateListener extends Listener {
   guildRepository: Repository<GuildEntity>;
+  userRepository: Repository<User>;
 
   constructor() {
     super("guildCreate", {
@@ -13,6 +15,7 @@ class GuildCreateListener extends Listener {
     });
 
     this.guildRepository = getRepository(GuildEntity);
+    this.userRepository = getRepository(User);
   }
 
   async exec(guild: Guild) {
@@ -20,6 +23,18 @@ class GuildCreateListener extends Listener {
       where: { discord_id: guild.id }
     });
     if (guildRecord) return;
+
+    guild.members.each(member => {
+      if (member.user.bot) return;
+      this.userRepository.findOne({ user_id: member.id }).then(user => {
+        if (!user) {
+          const userRecord = this.userRepository.create({
+            user_id: member.id
+          });
+          this.userRepository.save(userRecord);
+        }
+      });
+    });
 
     guildRecord = this.guildRepository.create({
       discord_id: guild.id
