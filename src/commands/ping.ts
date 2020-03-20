0685286160bbd99592ec1eb701f7a4cc8b8f2b1d
D3rise <derise2000@gmail.com>
2020-03-20 19:11:55 +0300

    Фикс бага с командой пинга

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -12,15 +12,25 @@ class PingCommand extends Command {
 
   async exec(message: Message) {
     const sent = await message.reply("секундочку...");
-    const timeDiff =
-      (sent.editedAt.getTime() || sent.createdAt.getTime()) -
-      (message.editedAt.getTime() || message.createdAt.getTime());
+<<<<<<< HEAD
+    const timeDiff = sent.createdAt.getTime() - message.createdAt.getTime();
     return message.channel.send(
       `🔂 **Время на отправку сообщения**: ${timeDiff} ms\n` +
         `💟 **Скорость ответа от Discord API**: ${Math.round(
           this.client.ws.ping
         )} ms`
     );
+=======
+    const timeDiff =
+      (sent.editedAt.getTime() || sent.createdAt.getTime()) -
+      (message.editedAt.getTime() || message.createdAt.getTime());
+    return message.util.reply([
+      `🔂 **Время на отправку сообщения**: ${timeDiff} ms`,
+      `💟 **Скорость ответа от Discord API**: ${Math.round(
+        this.client.ws.ping
+      )} ms`
+    ]);
+>>>>>>> parent of 0bde594... Фикс бага с командой пинга #2
   }
 }
 
